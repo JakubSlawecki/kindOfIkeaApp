c1d618f6bdc8cb4a77b4e062b3da61c3b6f9c0f3
Jakub Slawecki <jakubslawecki@gmail.com>
2018-11-07 18:54:06 +0100

    Initial Commit

---
+++ kindOfIkeaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kindOfIkeaApp
+//
+//  Created by Jakub Slawecki on 07/11/2018.
+//  Copyright © 2018 Jakub Slawecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
