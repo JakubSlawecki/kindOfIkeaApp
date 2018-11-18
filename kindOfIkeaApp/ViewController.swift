64cd509c414459cc0b8e6fe3729860358628b908
Jakub Slawecki <jakubslawecki@gmail.com>
2018-11-18 19:20:01 +0100

    Pinch and Pan gesture recognizer added

--- kindOfIkeaApp/ViewController.swift
+++ kindOfIkeaApp/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
     let itemsArray: [String] = ["Mug", "Banana", "Chair", "Table", "Lamp"]
+    @IBOutlet weak var planeDetectedLabel: UILabel!
     @IBOutlet weak var itemsCollectionsView: UICollectionView!
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
@@ -23,12 +24,18 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         self.sceneView.session.run(configuration)
         self.itemsCollectionsView.dataSource = self
         self.itemsCollectionsView.delegate = self
+        self.sceneView.delegate = self
         self.registerGestureRecognizers()
+        self.sceneView.autoenablesDefaultLighting = true
     }
     
     func registerGestureRecognizers() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(rotate))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        self.sceneView.addGestureRecognizer(panGestureRecognizer)
     }
     
     @objc func tapped(sender: UITapGestureRecognizer) {
@@ -37,6 +44,41 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
         if !hitTest.isEmpty {
             self.addItem(hitTestResult: hitTest.first!)
+        }
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let pinchLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(pinchLocation)
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            let node = results.node
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node.runAction(pinchAction)
+            sender.scale = 1.0
+        }
+    }
+    
+    @objc func rotate(sender: UIPanGestureRecognizer) {
+        let panLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(panLocation)
+        if !hitTest.isEmpty{
+            let result = hitTest.first!
+            if sender.state == .changed{
+                var translation = sender.translation(in: sceneView)
+                if translation.y >= 0{
+                    translation.y = 1.0
+                }else if translation.y >= 0{
+                    translation.y = 1.0
+                    let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(Double(-translation.y/70) * Double.pi), z: 0, duration: 0)
+                    result.node.runAction(rotation)
+                }else if translation.y  <= 0{
+                    translation.y = -1.0
+                    let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(Double(-translation.y/70) * Double.pi), z: 0, duration: 0)
+                    result.node.runAction(rotation)
+                }
+            }
         }
     }
     
@@ -70,6 +112,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
         cell?.backgroundColor = UIColor.lightGray
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetectedLabel.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.planeDetectedLabel.isHidden = true
+            }
+        }
     }
 
 
