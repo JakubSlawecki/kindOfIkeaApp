5a62f2cd4826791939de9e368cb05cfd690e1f39
Jakub Slawecki <jakubslawecki@gmail.com>
2018-11-18 13:03:18 +0100

    3d Models added

--- kindOfIkeaApp/ViewController.swift
+++ kindOfIkeaApp/ViewController.swift
@@ -10,17 +10,45 @@ import UIKit
 import ARKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    let itemsArray: [String] = ["Cup", "Vase", "Chair", "Table", "Lamp"]
+    let itemsArray: [String] = ["Mug", "Banana", "Chair", "Table", "Lamp"]
     @IBOutlet weak var itemsCollectionsView: UICollectionView!
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.itemsCollectionsView.dataSource = self
         self.itemsCollectionsView.delegate = self
+        self.registerGestureRecognizers()
+    }
+    
+    func registerGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        if !hitTest.isEmpty {
+            self.addItem(hitTestResult: hitTest.first!)
+        }
+    }
+    
+    func addItem(hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem {
+        let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn")
+        let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: true))!
+        let transform = hitTestResult.worldTransform
+        let thirdColumn = transform.columns.3
+        node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -35,6 +63,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
         cell?.backgroundColor = UIColor.darkGray
     }
     
